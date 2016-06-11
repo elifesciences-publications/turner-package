c6a5d4339a0a2655f0f10c02d85ef704d2227b01
mhturner <maxwellholteturner@gmail.com>
2016-06-10 17:29:50 -0700

    CheckerboardNoise and associated STRF figure

---
+++ +edu/+washington/+riekelab/+turner/+figures/StrfFigure.m
@@ -0,0 +1,173 @@
+classdef StrfFigure < symphonyui.core.FigureHandler
+    
+    properties (SetAccess = private)
+        ampDevice
+        frameMonitor
+        stageDevice
+        recordingType
+        preTime
+        stimTime
+        frameDwell
+        seedID
+        binaryNoise
+    end
+    
+    properties (Access = private)
+        axesHandle
+        imHandle
+        noiseStream
+        newFilter
+        epochCount
+    end
+    
+    methods
+        
+        function obj = StrfFigure(ampDevice, frameMonitor, stageDevice, varargin)
+            obj.ampDevice = ampDevice;
+            obj.frameMonitor = frameMonitor;
+            obj.stageDevice = stageDevice;
+            ip = inputParser();
+            ip.addParameter('recordingType', [], @(x)ischar(x));
+            ip.addParameter('preTime', [], @(x)isvector(x));
+            ip.addParameter('stimTime', [], @(x)isvector(x));
+            ip.addParameter('frameDwell', [], @(x)isvector(x));
+            ip.addParameter('seedID', 'noiseSeed', @(x)ischar(x));
+            ip.addParameter('binaryNoise', true, @(x)islogical(x));
+            ip.parse(varargin{:});
+            
+            obj.recordingType = ip.Results.recordingType;
+            obj.preTime = ip.Results.preTime;
+            obj.stimTime = ip.Results.stimTime;
+            obj.frameDwell = ip.Results.frameDwell;
+            obj.seedID = ip.Results.seedID;
+            obj.binaryNoise = ip.Results.binaryNoise;
+
+            obj.epochCount = 0;
+            obj.newFilter = 0;
+            obj.createUi();
+        end
+
+        function createUi(obj)
+            import appbox.*;
+            toolbar = findall(obj.figureHandle, 'Type', 'uitoolbar');
+            playStrfButton = uipushtool( ...
+                'Parent', toolbar, ...
+                'TooltipString', 'Play Strf movie', ...
+                'Separator', 'on', ...
+                'ClickedCallback', @obj.onSelectedPlayStrf);
+            setIconImage(playStrfButton, symphonyui.app.App.getResource('icons/view_only.png'));
+
+            obj.axesHandle = axes( ...
+                'Parent', obj.figureHandle, ...
+                'FontName', get(obj.figureHandle, 'DefaultUicontrolFontName'), ...
+                'FontSize', get(obj.figureHandle, 'DefaultUicontrolFontSize'), ...
+                'XTickMode', 'auto');
+            xlabel(obj.axesHandle, '');
+            ylabel(obj.axesHandle, '');
+            
+
+            obj.figureHandle.Name ='STRF';
+        end
+
+        function handleEpoch(obj, epoch)
+            obj.epochCount = obj.epochCount + 1;
+            %load amp data
+            response = epoch.getResponse(obj.ampDevice);
+            epochResponseTrace = response.getData();
+            sampleRate = response.sampleRate.quantityInBaseUnits;
+            prePts = sampleRate*obj.preTime/1000;
+            if strcmp(obj.recordingType,'extracellular') %spike recording
+                newResponse = zeros(size(epochResponseTrace));
+                %count spikes
+                S = edu.washington.riekelab.turner.utils.spikeDetectorOnline(epochResponseTrace);
+                newResponse(S.sp) = 1;
+            else %intracellular - Vclamp
+                epochResponseTrace = epochResponseTrace-mean(epochResponseTrace(1:prePts)); %baseline
+                if strcmp(obj.recordingType,'exc') %measuring exc
+                    polarity = -1;
+                elseif strcmp(obj.recordingType,'inh') %measuring inh
+                    polarity = 1;
+                end
+                newResponse = polarity * epochResponseTrace;
+            end
+            %load frame monitor data
+            if isa(obj.stageDevice,'edu.washington.riekelab.devices.LightCrafterDevice')
+                lightCrafterFlag = 1;
+            else %OLED stage device
+                lightCrafterFlag = 0;
+            end
+            frameRate = obj.stageDevice.getMonitorRefreshRate();
+            FMresponse = epoch.getResponse(obj.frameMonitor);
+            FMdata = FMresponse.getData();
+            frameTimes = edu.washington.riekelab.turner.utils.getFrameTiming(FMdata,lightCrafterFlag);
+            preFrames = frameRate*(obj.preTime/1000);
+            firstStimFrameFlip = frameTimes(preFrames+1);
+            newResponse = newResponse(firstStimFrameFlip:end); %cut out pre-frames
+            
+            %reconstruct noise stimulus
+            filterLen = 800; %msec, length of linear filter to compute
+            %fraction of noise update rate at which to cut off filter spectrum
+            freqCutoffFraction = 0.8;
+            
+            currentNoiseSeed = epoch.parameters(obj.seedID);
+            numChecksX = epoch.parameters('numChecksX');
+            numChecksY = epoch.parameters('numChecksY');
+            %reconstruct stimulus trajectories...
+            stimFrames = round(frameRate * (obj.stimTime/1e3));
+            response = zeros(1, floor(stimFrames/obj.frameDwell));
+            %reset random stream to recover stim trajectories
+            obj.noiseStream = RandStream('mt19937ar', 'Seed', currentNoiseSeed);
+            % get stim trajectories and response in frame updates
+            chunkLen = obj.frameDwell*mean(diff(frameTimes));
+            noiseMatrix = zeros(numChecksY,numChecksX,floor(stimFrames/obj.frameDwell));
+            for ii = 1:floor(stimFrames/obj.frameDwell)
+                if (obj.binaryNoise)
+                    noiseMatrix(:,:,ii) = ... 
+                        obj.noiseStream.rand(numChecksY,numChecksX) > 0.5;
+                else
+                    noiseMatrix(:,:,ii) = ... 
+                        obj.noiseStream.randn(numChecksY,numChecksX);
+                end
+                response(ii) = mean(newResponse(round((ii-1)*chunkLen + 1) : round(ii*chunkLen)));
+            end
+            updateRate = (frameRate/obj.frameDwell); %hz
+            
+            filterPts = (filterLen/1000)*updateRate;
+            
+            filterTmp = zeros(numChecksY,numChecksX,filterPts);
+            for b = 1 : size(noiseMatrix,1)
+                for c = 1 : size(noiseMatrix,2)
+                    tmp = edu.washington.riekelab.turner.utils.getLinearFilterOnline(...
+                        squeeze(noiseMatrix(b,c,:))',...
+                        response,updateRate, freqCutoffFraction*updateRate);
+                    filterTmp(b,c,:) = tmp(1:filterPts);
+                end
+            end
+            %normalize within each epoch
+            filterTmp = filterTmp ./ max(filterTmp(:));
+            obj.newFilter = ((obj.epochCount - 1)*obj.newFilter + filterTmp) / obj.epochCount;
+            
+            filterTimes = linspace(0,filterLen,filterPts); %msec
+            targetTime = 30; %msec
+            [~, snapShotInd] = min(abs(filterTimes - targetTime));
+            if isempty(obj.imHandle)
+                obj.imHandle = imagesc(obj.newFilter(:,:,snapShotInd),...
+                    'Parent', obj.axesHandle);
+                title(obj.axesHandle,['Strf at ', num2str(filterTimes(snapShotInd)) ' msec']);
+                colormap(obj.axesHandle, gray)
+            else
+                set(obj.imHandle, 'CData', obj.newFilter(:,:,2));
+            end
+        end
+        
+    end
+    
+    methods (Access = private)
+        
+        function onSelectedPlayStrf(obj, ~, ~)
+            implay(obj.newFilter);
+        end
+    end
+
+end
+
---
+++ +edu/+washington/+riekelab/+turner/+protocols/CheckerboardNoise.m
@@ -0,0 +1,132 @@
+classdef CheckerboardNoise < edu.washington.riekelab.protocols.RiekeLabStageProtocol
+    
+    properties
+        preTime = 500 % ms
+        stimTime = 20000 % ms
+        tailTime = 500 % ms
+        stixelSize = 30 % um
+        binaryNoise = true %binary checkers - overrides noiseStdv
+        noiseStdv = 0.3 %contrast, as fraction of mean
+        frameDwell = 1 % Frames per noise update
+        useRandomSeed = true % false = repeated noise trajectory (seed 0)
+        centerOffset = [0,0];
+        backgroundIntensity = 0.5 % (0-1)
+        onlineAnalysis = 'none'
+        numberOfAverages = uint16(20) % number of epochs to queue
+        amp % Output amplifier
+    end
+
+    properties (Hidden)
+        ampType
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'})
+        noiseSeed
+        noiseStream
+        numChecksX
+        numChecksY
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+         
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.turner.figures.FrameTimingFigure',...
+                obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+            if ~strcmp(obj.onlineAnalysis,'none')
+                obj.showFigure('edu.washington.riekelab.turner.figures.StrfFigure',...
+                obj.rig.getDevice(obj.amp),obj.rig.getDevice('Frame Monitor'),...
+                obj.rig.getDevice('Stage'),...
+                'recordingType',obj.onlineAnalysis,...
+                'preTime',obj.preTime,'stimTime',obj.stimTime,...
+                'frameDwell',obj.frameDwell,'binaryNoise',obj.binaryNoise);
+            end
+            
+            %get number of checkers...
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            %convert from microns to pixels...
+            stixelSizePix = obj.rig.getDevice('Stage').um2pix(obj.stixelSize);
+            obj.numChecksX = round(canvasSize(1) / stixelSizePix);
+            obj.numChecksY = round(canvasSize(2) / stixelSizePix);
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
+            device = obj.rig.getDevice(obj.amp);
+            duration = (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
+            epoch.addResponse(device);
+            
+            % Determine seed values.
+            if obj.useRandomSeed
+                obj.noiseSeed = RandStream.shuffleSeed;
+            else
+                obj.noiseSeed = 0;
+            end
+            
+            %at start of epoch, set random stream
+            obj.noiseStream = RandStream('mt19937ar', 'Seed', obj.noiseSeed);
+            epoch.addParameter('noiseSeed', obj.noiseSeed);
+            epoch.addParameter('numChecksX', obj.numChecksX);
+            epoch.addParameter('numChecksY', obj.numChecksY);
+        end
+
+        function p = createPresentation(obj)
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3); %create presentation of specified duration
+            p.setBackgroundColor(obj.backgroundIntensity); % Set background intensity
+            
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            centerOffsetPix = obj.rig.getDevice('Stage').um2pix(obj.centerOffset);
+            
+            % Create checkerboard
+            initMatrix = uint8(255.*(obj.backgroundIntensity .* ones(obj.numChecksY,obj.numChecksX)));
+            board = stage.builtin.stimuli.Image(initMatrix);
+            board.size = canvasSize;
+            board.position = canvasSize/2 + centerOffsetPix;
+            board.setMinFunction(GL.NEAREST); %don't interpolate to scale up board
+            board.setMagFunction(GL.NEAREST);
+            p.addStimulus(board);
+            preFrames = round(60 * (obj.preTime/1e3));
+            checkerboardController = stage.builtin.controllers.PropertyController(board, 'imageMatrix',...
+                @(state)getNewCheckerboard(obj, state.frame - preFrames));
+            p.addController(checkerboardController); %add the controller
+            function i = getNewCheckerboard(obj, frame)
+                persistent boardMatrix;
+                if frame<0 %pre frames. frame 0 starts stimPts
+                    boardMatrix = obj.backgroundIntensity;
+                else %in stim frames
+                    if mod(frame, obj.frameDwell) == 0 %noise update
+                        if (obj.binaryNoise)
+                            boardMatrix = 2*obj.backgroundIntensity * ...
+                                (obj.noiseStream.rand(obj.numChecksY,obj.numChecksX) > 0.5);
+                        else
+                            boardMatrix = obj.backgroundIntensity + ...
+                                obj.noiseStdv * obj.backgroundIntensity * ...
+                                obj.noiseStream.randn(obj.numChecksY,obj.numChecksX);
+                        end
+                    end
+                end
+                i = uint8(255 * boardMatrix);
+            end
+
+            % hide during pre & post
+            boardVisible = stage.builtin.controllers.PropertyController(board, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(boardVisible); 
+        end
+
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+    end
+    
+end
