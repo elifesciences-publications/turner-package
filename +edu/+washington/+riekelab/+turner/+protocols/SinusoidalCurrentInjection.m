64ee94c93d2eb3e00909b25808b470b1595feecc
mturner <mhturner@uw.edu>
2016-07-18 15:12:49 -0700

    New stim - sine current injection

---
+++ +edu/+washington/+riekelab/+turner/+protocols/SinusoidalCurrentInjection.m
@@ -0,0 +1,82 @@
+classdef SinusoidalCurrentInjection < edu.washington.riekelab.protocols.RiekeLabProtocol
+    
+    properties
+        amp                             % Output amplifier
+        preTime = 100                    % Sine leading duration (ms)
+        stimTime = 3000                  % Sine duration (ms)
+        tailTime = 100                   % Sine trailing duration (ms)
+        sineAmplitude = 20              % Sine amplitude (mV or pA)
+        sineFrequency = 4               % Sine frequency (Hz)
+        numberOfAverages = uint16(5)    % Number of epochs
+        interpulseInterval = 0          % Duration between ramps (s)
+    end
+    
+    properties (Hidden)
+        ampType
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function p = getPreview(obj, panel)
+            p = symphonyui.builtin.previews.StimuliPreview(panel, @()obj.createAmpStimulus());
+        end
+        
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.turner.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp));
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseStatisticsFigure', obj.rig.getDevice(obj.amp), {@mean, @var}, ...
+                'baselineRegion', [0 obj.preTime], ...
+                'measurementRegion', [obj.preTime obj.preTime+obj.stimTime]);
+        end
+        
+        function stim = createAmpStimulus(obj)
+            gen = symphonyui.builtin.stimuli.SineGenerator();
+            
+            gen.preTime = obj.preTime;
+            gen.stimTime = obj.stimTime;
+            gen.tailTime = obj.tailTime;
+            gen.amplitude = obj.sineAmplitude;
+            gen.period = (1 / obj.sineFrequency) * obj.sampleRate; %in msec
+            gen.mean = obj.rig.getDevice(obj.amp).background.quantity;
+            gen.sampleRate = obj.sampleRate;
+            gen.units = obj.rig.getDevice(obj.amp).background.displayUnits;
+            
+            stim = gen.generate();
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, epoch);
+            
+            epoch.addStimulus(obj.rig.getDevice(obj.amp), obj.createAmpStimulus());
+            epoch.addResponse(obj.rig.getDevice(obj.amp));
+        end
+        
+        function prepareInterval(obj, interval)
+            prepareInterval@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, interval);
+            
+            device = obj.rig.getDevice(obj.amp);
+            interval.addDirectCurrentStimulus(device, device.background, obj.interpulseInterval, obj.sampleRate);
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+        
+    end
+    
+end
+
