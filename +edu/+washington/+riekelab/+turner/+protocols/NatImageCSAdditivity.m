7d4dbc6b832df9e86a2f41973bd2d37d19da3353
Max Turner <mturner@uw.edu>
2016-08-12 17:07:04 -0700

    New stim - NatImageCSAdditivity

---
+++ +edu/+washington/+riekelab/+turner/+protocols/NatImageCSAdditivity.m
@@ -0,0 +1,227 @@
+classdef NatImageCSAdditivity < edu.washington.riekelab.protocols.RiekeLabStageProtocol
+
+    properties
+        preTime = 200 % ms
+        stimTime = 200 % ms
+        tailTime = 200 % ms
+        imageName = '00152' %van hateren image names
+        seed = 1 % rand seed for picking image patches
+        noPatches = 30 %number of different image patches (fixations) to show
+        centerDiameter = 200 % um
+        annulusInnerDiameter = 300 % um
+        annulusOuterDiameter = 600 % um
+        patchSampling = 'random'
+        patchContrast = 'all'
+        centerOffset = [0, 0] % [x,y] (um)
+        onlineAnalysis = 'none'
+        numberOfAverages = uint16(270) % number of epochs to queue
+        amp % Output amplifier
+    end
+    
+    properties (Hidden)
+        ampType
+        imageNameType = symphonyui.core.PropertyType('char', 'row', {'00152','00377','00405','00459','00657','01151','01154',...
+            '01192','01769','01829','02265','02281','02733','02999','03093',...
+            '03347','03447','03584','03758','03760'})
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'exc', 'inh'})
+        patchSamplingType = symphonyui.core.PropertyType('char', 'row', {'random','ranked'})
+        patchContrastType = symphonyui.core.PropertyType('char', 'row', {'all','negative','positive'})
+        centerOffsetType = symphonyui.core.PropertyType('denserealdouble', 'matrix')
+        
+        wholeImageMatrix
+        imagePatchMatrix
+        patchLocations
+        
+        %saved out to each epoch...
+        currentStimSet
+        backgroundIntensity
+        imagePatchIndex
+        currentPatchLocation
+        currentStimulus
+    end
+
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            obj.showFigure('edu.washington.riekelab.turner.figures.MeanResponseFigure',...
+                obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,...
+                'groupBy',{'currentStimulus'});
+            obj.showFigure('edu.washington.riekelab.turner.figures.FrameTimingFigure',...
+                obj.rig.getDevice('Stage'), obj.rig.getDevice('Frame Monitor'));
+            
+            %load appropriate image...
+            resourcesDir = 'C:\Users\Public\Documents\turner-package\resources\';
+            obj.currentStimSet = '/VHsubsample_20160105';
+            fileId=fopen([resourcesDir, obj.currentStimSet, '/imk', obj.imageName,'.iml'],'rb','ieee-be');
+            img = fread(fileId, [1536,1024], 'uint16');
+           
+            img = double(img);
+            img = (img./max(img(:))); %rescale s.t. brightest point is maximum monitor level
+            obj.backgroundIntensity = mean(img(:));%set the mean to the mean over the image
+            img = img.*255; %rescale s.t. brightest point is maximum monitor level
+            obj.wholeImageMatrix = uint8(img);
+            rng(obj.seed); %set random seed for fixation draw
+            
+            %get patch locations:
+            load([resourcesDir,'NaturalImageFlashLibrary_072216.mat']);
+            fieldName = ['imk', obj.imageName];
+            %1) restrict to desired patch contrast:
+            LnResp = imageData.(fieldName).LnModelResponse;
+            if strcmp(obj.patchContrast,'all')
+                inds = 1:length(LnResp);
+            elseif strcmp(obj.patchContrast,'positive')
+                inds = find(LnResp > 0);
+            elseif strcmp(obj.patchContrast,'negative')
+                inds = find(LnResp <= 0);
+            end
+            xLoc = imageData.(fieldName).location(inds,1);
+            yLoc = imageData.(fieldName).location(inds,2);
+            subunitResp = imageData.(fieldName).SubunitModelResponse(inds);
+            LnResp = imageData.(fieldName).LnModelResponse(inds);
+            
+            %2) do patch sampling:
+            responseDifferences = subunitResp - LnResp;
+            if strcmp(obj.patchSampling,'random')
+                %get patch indices:
+                pullInds = randsample(1:length(xLoc),obj.noPatches);
+            else strcmp(obj.patchSampling,'ranked')
+                %pull more than needed to account for empty bins at tail
+                [~, ~, bin] = histcounts(responseDifferences,1.5*obj.noPatches);
+                populatedBins = unique(bin);
+                %pluck one patch from each bin
+                pullInds = arrayfun(@(b) find(b == bin,1),populatedBins);
+                %get patch indices:
+                pullInds = randsample(pullInds,obj.noPatches);
+            end
+            obj.patchLocations(1,1:obj.noPatches) = xLoc(pullInds); %in VH pixels
+            obj.patchLocations(2,1:obj.noPatches) = yLoc(pullInds);
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj, epoch);
+
+            %pull patch location:
+            obj.imagePatchIndex = floor(mod(obj.numEpochsCompleted/3,obj.noPatches) + 1);
+            stimInd = mod(obj.numEpochsCompleted,3);
+            if stimInd == 0
+                obj.currentStimulus = 'Center';
+            elseif stimInd == 1
+                obj.currentStimulus = 'Surround';
+            elseif stimInd == 2
+                obj.currentStimulus = 'Center-Surround';
+            end
+            
+            obj.currentPatchLocation(1) = obj.patchLocations(1,obj.imagePatchIndex); %in VH pixels
+            obj.currentPatchLocation(2) = obj.patchLocations(2,obj.imagePatchIndex);
+            
+            device = obj.rig.getDevice(obj.amp);
+            duration = (obj.preTime + obj.stimTime + obj.tailTime) / 1e3;
+            epoch.addDirectCurrentStimulus(device, device.background, duration, obj.sampleRate);
+            epoch.addResponse(device);
+            
+            %imagePatchMatrix is in VH pixels
+            %size of the stimulus on the prep:
+            stimSize = obj.rig.getDevice('Stage').getCanvasSize() .* ...
+                obj.rig.getDevice('Stage').getConfigurationSetting('micronsPerPixel'); %um
+            stimSize_VHpix = stimSize ./ (3.3); %um / (um/pixel) -> pixel
+            radX = stimSize_VHpix(1) / 2; %boundaries for fixation draws depend on stimulus size
+            radY = stimSize_VHpix(2) / 2;
+            obj.imagePatchMatrix = obj.wholeImageMatrix(round(obj.currentPatchLocation(1)-radX):round(obj.currentPatchLocation(1)+radX),...
+                round(obj.currentPatchLocation(2)-radY):round(obj.currentPatchLocation(2)+radY));
+            obj.imagePatchMatrix = obj.imagePatchMatrix';
+
+            epoch.addParameter('currentStimSet', obj.currentStimSet);
+            epoch.addParameter('backgroundIntensity', obj.backgroundIntensity);
+            epoch.addParameter('imagePatchIndex', obj.imagePatchIndex);
+            epoch.addParameter('currentPatchLocation', obj.currentPatchLocation);
+            epoch.addParameter('currentStimulus', obj.currentStimulus);
+        end
+        
+        function p = createPresentation(obj)
+            canvasSize = obj.rig.getDevice('Stage').getCanvasSize();
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            p.setBackgroundColor(obj.backgroundIntensity);
+            
+            centerDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.centerDiameter);
+            annulusInnerDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.annulusInnerDiameter);
+            annulusOuterDiameterPix = obj.rig.getDevice('Stage').um2pix(obj.annulusOuterDiameter);
+            centerOffsetPix = obj.rig.getDevice('Stage').um2pix(obj.centerOffset);
+            
+            %make image patch:
+            scene = stage.builtin.stimuli.Image(obj.imagePatchMatrix);
+            scene.size = canvasSize; %scale up to canvas size
+            scene.position = canvasSize/2 + centerOffsetPix;
+            % Use linear interpolation when scaling the image.
+            scene.setMinFunction(GL.LINEAR);
+            scene.setMagFunction(GL.LINEAR);
+            p.addStimulus(scene);
+            sceneVisible = stage.builtin.controllers.PropertyController(scene, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(sceneVisible);
+            
+            % occlude with appropriate aperture / mask / annulus
+            if strcmp(obj.currentStimulus,'Center') %aperture around center
+                aperture = stage.builtin.stimuli.Rectangle();
+                aperture.position = canvasSize/2 + centerOffsetPix;
+                aperture.color = obj.backgroundIntensity;
+                aperture.size = [max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createCircularAperture(centerDiameterPix/max(canvasSize), 1024);
+                aperture.setMask(mask);
+                p.addStimulus(aperture);
+            elseif strcmp(obj.currentStimulus,'Surround') %aperture in far surround + mask in center
+                % big aperture:
+                aperture = stage.builtin.stimuli.Rectangle();
+                aperture.position = canvasSize/2 + centerOffsetPix;
+                aperture.color = obj.backgroundIntensity;
+                aperture.size = [max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createCircularAperture(annulusOuterDiameterPix/max(canvasSize), 1024);
+                aperture.setMask(mask);
+                p.addStimulus(aperture);
+                % center mask:
+                maskSpot = stage.builtin.stimuli.Ellipse();
+                maskSpot.radiusX = annulusInnerDiameterPix/2;
+                maskSpot.radiusY = annulusInnerDiameterPix/2;
+                maskSpot.position = canvasSize/2 + centerOffsetPix;
+                maskSpot.color = obj.backgroundIntensity;
+                p.addStimulus(maskSpot);
+            elseif strcmp(obj.currentStimulus,'Center-Surround') %annulus between center & surround
+                % big aperture:
+                aperture = stage.builtin.stimuli.Rectangle();
+                aperture.position = canvasSize/2 + centerOffsetPix;
+                aperture.color = obj.backgroundIntensity;
+                aperture.size = [max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createCircularAperture(annulusOuterDiameterPix/max(canvasSize), 1024);
+                aperture.setMask(mask);
+                p.addStimulus(aperture);
+                
+                %annulus between center & surround:
+                annulus = stage.builtin.stimuli.Rectangle();
+                annulus.position = canvasSize/2 + centerOffsetPix;
+                annulus.color = obj.backgroundIntensity;
+                annulus.size = [max(canvasSize) max(canvasSize)];
+                mask = stage.core.Mask.createAnnulus(centerDiameterPix/max(canvasSize),...
+                    annulusInnerDiameterPix/max(canvasSize),1024);
+                annulus.setMask(mask);
+                p.addStimulus(annulus);
+            end
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+
+    end
+    
+end
