aaf03144e770faf98b679b3e432c52dc7a9a64a3
mturner <mhturner@uw.edu>
2016-10-23 11:35:17 -0700

    new texture util

---
+++ +edu/+washington/+riekelab/+turner/+utils/makeTextureMatrix.m
@@ -0,0 +1,20 @@
+function mat = makeTextureMatrix(textureSize, sigma, randSeed, meanIntensity)
+% set random seed
+stream = RandStream('mt19937ar','Seed',randSeed);
+mat = double(rand(stream, [textureSize,textureSize]));
+% make gaussian filter
+h = fspecial('gaussian',6*sigma,sigma);
+h = h ./ sum(h(:)); % normalize
+mat = imfilter(mat,h,'replicate');
+
+% make histogram of pixel values uniform
+% From Schwartz lab
+bins = [-Inf prctile(mat(:),1:1:100)];
+m_orig = mat;
+for bb=1:length(bins)-1
+    mat(m_orig>bins(bb) & m_orig<=bins(bb+1)) = bb*(1/(length(bins)-1));
+end
+% final little scaling adjustments
+mat = mat - min(mat(:));
+mat = 2 * meanIntensity .* (mat ./ max(mat(:)));
+end
