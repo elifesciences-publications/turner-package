1e42e7dce922c39b796f6504ee51938238d22b26
mturner <mhturner@uw.edu>
2017-06-30 12:13:49 -0700

    New laser scan protocol and rig

---
+++ +edu/+washington/+riekelab/+turner/+protocols/PulseWithScanTrigger.m
@@ -0,0 +1,155 @@
+classdef PulseWithScanTrigger < edu.washington.riekelab.protocols.RiekeLabProtocol
+    % Presents a set of rectangular pulse stimuli to a specified amplifier and records from the same amplifier.
+    % Added an output TTL pulse to trigger laser scanning
+    
+    properties
+        amp                             % Output amplifier
+        preTime = 50                    % Pulse leading duration (ms)
+        stimTime = 500                  % Pulse duration (ms)
+        tailTime = 50                   % Pulse trailing duration (ms)
+        pulseAmplitude = 100            % Pulse amplitude (mV or pA depending on amp mode)
+    end
+    
+    properties (Dependent, SetAccess = private)
+        amp2                            % Secondary amplifier
+    end
+    
+    properties
+        amp2PulseAmplitude = 0          % Pulse amplitude for secondary amp (mV or pA depending on amp2 mode)
+        numberOfAverages = uint16(5)    % Number of epochs
+        interpulseInterval = 0          % Duration between pulses (s)
+    end
+    
+    properties (Hidden)
+        ampType
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function d = getPropertyDescriptor(obj, name)
+            d = getPropertyDescriptor@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, name);
+            
+            if strncmp(name, 'amp2', 4) && numel(obj.rig.getDeviceNames('Amp')) < 2
+                d.isHidden = true;
+            end
+        end
+        
+        function p = getPreview(obj, panel)
+            p = symphonyui.builtin.previews.StimuliPreview(panel, @()obj.createAmpStimulus());
+        end
+        
+        function prepareRun(obj)
+            prepareRun@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
+            
+            if numel(obj.rig.getDeviceNames('Amp')) < 2
+                obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+                obj.showFigure('symphonyui.builtin.figures.MeanResponseFigure', obj.rig.getDevice(obj.amp));
+                obj.showFigure('symphonyui.builtin.figures.ResponseStatisticsFigure', obj.rig.getDevice(obj.amp), {@mean, @var}, ...
+                    'baselineRegion', [0 obj.preTime], ...
+                    'measurementRegion', [obj.preTime obj.preTime+obj.stimTime]);
+            else
+                obj.showFigure('edu.washington.riekelab.figures.DualResponseFigure', obj.rig.getDevice(obj.amp), obj.rig.getDevice(obj.amp2));
+                obj.showFigure('edu.washington.riekelab.figures.DualMeanResponseFigure', obj.rig.getDevice(obj.amp), obj.rig.getDevice(obj.amp2));
+                obj.showFigure('edu.washington.riekelab.figures.DualResponseStatisticsFigure', obj.rig.getDevice(obj.amp), {@mean, @var}, obj.rig.getDevice(obj.amp2), {@mean, @var}, ...
+                    'baselineRegion1', [0 obj.preTime], ...
+                    'measurementRegion1', [obj.preTime obj.preTime+obj.stimTime], ...
+                    'baselineRegion2', [0 obj.preTime], ...
+                    'measurementRegion2', [obj.preTime obj.preTime+obj.stimTime]);
+            end
+        end
+        
+        function stim = createAmpStimulus(obj)
+            gen = symphonyui.builtin.stimuli.PulseGenerator();
+            
+            gen.preTime = obj.preTime;
+            gen.stimTime = obj.stimTime;
+            gen.tailTime = obj.tailTime;
+            gen.amplitude = obj.pulseAmplitude;
+            gen.mean = obj.rig.getDevice(obj.amp).background.quantity;
+            gen.sampleRate = obj.sampleRate;
+            gen.units = obj.rig.getDevice(obj.amp).background.displayUnits;
+            
+            stim = gen.generate();
+        end
+        
+        function stim = createAmp2Stimulus(obj)
+            gen = symphonyui.builtin.stimuli.PulseGenerator();
+            
+            gen.preTime = obj.preTime;
+            gen.stimTime = obj.stimTime;
+            gen.tailTime = obj.tailTime;
+            gen.amplitude = obj.amp2PulseAmplitude;
+            gen.mean = obj.rig.getDevice(obj.amp2).background.quantity;
+            gen.sampleRate = obj.sampleRate;
+            gen.units = obj.rig.getDevice(obj.amp2).background.displayUnits;
+            
+            stim = gen.generate();
+        end
+        
+        function stim = createScanTriggerStimulus(obj)
+            gen = symphonyui.builtin.stimuli.RepeatingPulseGenerator();
+            
+            gen.preTime = 0;
+            gen.stimTime = 1;
+            gen.tailTime = obj.preTime + obj.stimTime + obj.tailTime - 1;
+            gen.amplitude = 1;
+            gen.mean = 0;
+            gen.sampleRate = obj.sampleRate;
+            gen.units = symphonyui.core.Measurement.UNITLESS;
+            
+            stim = gen.generate();
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, epoch);
+            
+            epoch.addStimulus(obj.rig.getDevice(obj.amp), obj.createAmpStimulus());
+            epoch.addResponse(obj.rig.getDevice(obj.amp));
+            
+            
+            triggers = obj.rig.getDevices('Scan Trigger');
+            if ~isempty(triggers)            
+                epoch.addStimulus(triggers{1}, obj.createScanTriggerStimulus());
+            end
+            
+            if numel(obj.rig.getDeviceNames('Amp')) >= 2
+                epoch.addStimulus(obj.rig.getDevice(obj.amp2), obj.createAmp2Stimulus());
+                epoch.addResponse(obj.rig.getDevice(obj.amp2));
+            end
+        end
+        
+        function prepareInterval(obj, interval)
+            prepareInterval@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, interval);
+            
+            device = obj.rig.getDevice(obj.amp);
+            interval.addDirectCurrentStimulus(device, device.background, obj.interpulseInterval, obj.sampleRate);
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+        
+        function a = get.amp2(obj)
+            amps = obj.rig.getDeviceNames('Amp');
+            if numel(amps) < 2
+                a = '(None)';
+            else
+                i = find(~ismember(amps, obj.amp), 1);
+                a = amps{i};
+            end
+        end
+        
+    end
+    
+end
+
---
+++ +edu/+washington/+riekelab/+turner/+rigs/SharedTwoPhotonWithScanTrigger.m
@@ -0,0 +1,74 @@
+classdef SharedTwoPhotonWithScanTrigger < symphonyui.core.descriptions.RigDescription
+    
+    methods
+        
+        function obj = SharedTwoPhotonWithScanTrigger()
+            import symphonyui.builtin.daqs.*;
+            import symphonyui.builtin.devices.*;
+            import symphonyui.core.*;
+            import edu.washington.*;
+            
+            daq = HekaDaqController();
+            obj.daqController = daq;
+            
+            amp1 = MultiClampDevice('Amp1', 1).bindStream(daq.getStream('ao0')).bindStream(daq.getStream('ai0'));
+            obj.addDevice(amp1);
+            
+            redRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'red_led_gamma_ramp.txt'));
+            red = CalibratedDevice('Red LED', Measurement.NORMALIZED, redRamp(:, 1), redRamp(:, 2)).bindStream(daq.getStream('ao1'));
+            red.addConfigurationSetting('ndfs', {}, ...
+                'type', PropertyType('cellstr', 'row', {'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9'}));
+            red.addResource('ndfAttenuations', containers.Map( ...
+                {'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9'}, ...
+                {0.9884, 0.9910, 1.9023, 2.0200, 3.9784, 0.3, 0.6, 1.14, 1.99}));
+            red.addResource('fluxFactorPaths', containers.Map( ...
+                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'red_led_flux_factors.txt')}));
+            red.addConfigurationSetting('lightPath', '', ...
+                'type', PropertyType('char', 'row', {'', 'below', 'above'}));
+            red.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'red_led_spectrum.txt')));            
+            obj.addDevice(red);
+            
+            uvRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'uv_led_gamma_ramp.txt'));
+            uv = CalibratedDevice('UV LED', Measurement.NORMALIZED, uvRamp(:, 1), uvRamp(:, 2)).bindStream(daq.getStream('ao2'));
+            uv.addConfigurationSetting('ndfs', {}, ...
+                'type', PropertyType('cellstr', 'row', {'G1', 'G2', 'G3', 'G4', 'G6', 'G7', 'G8', 'G9'}));
+            uv.addResource('ndfAttenuations', containers.Map( ...
+                {'G1', 'G2', 'G3', 'G4', 'G6', 'G7', 'G8', 'G9'}, ...
+                {1.0060, 1.0524, 2.1342, 2.6278, 0.28, 0.59, 1.25, 2.23}));
+            uv.addResource('fluxFactorPaths', containers.Map( ...
+                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'uv_led_flux_factors.txt')}));
+            uv.addConfigurationSetting('lightPath', '', ...
+                'type', PropertyType('char', 'row', {'', 'below', 'above'}));
+            uv.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'uv_led_spectrum.txt')));          
+            obj.addDevice(uv);
+            
+            blueRamp = importdata(riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'blue_led_gamma_ramp.txt'));
+            blue = CalibratedDevice('Blue LED', Measurement.NORMALIZED, blueRamp(:, 1), blueRamp(:, 2)).bindStream(daq.getStream('ao3'));
+            blue.addConfigurationSetting('ndfs', {}, ...
+                'type', PropertyType('cellstr', 'row', {'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9'}));
+            blue.addResource('ndfAttenuations', containers.Map( ...
+                {'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9'}, ...
+                {1.0171, 1.0428, 2.0749, 2.1623, 4.2439, 0.26, 0.61, 1.22, 2.17}));
+            blue.addResource('fluxFactorPaths', containers.Map( ...
+                {'none'}, {riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'blue_led_flux_factors.txt')}));
+            blue.addConfigurationSetting('lightPath', '', ...
+                'type', PropertyType('char', 'row', {'', 'below', 'above'}));
+            blue.addResource('spectrum', importdata(riekelab.Package.getCalibrationResource('rigs', 'shared_two_photon', 'blue_led_spectrum.txt')));                       
+            obj.addDevice(blue);
+            
+            temperature = UnitConvertingDevice('Temperature Controller', 'V', 'manufacturer', 'Warner Instruments').bindStream(daq.getStream('ai6'));
+            obj.addDevice(temperature);
+            
+            trigger = UnitConvertingDevice('Oscilloscope Trigger', Measurement.UNITLESS).bindStream(daq.getStream('doport1'));
+            daq.getStream('doport1').setBitPosition(trigger, 0);
+            obj.addDevice(trigger);
+            
+            scanTrigger = UnitConvertingDevice('Scan Trigger', Measurement.UNITLESS).bindStream(daq.getStream('doport2'));
+            daq.getStream('doport2').setBitPosition(scanTrigger, 0);
+            obj.addDevice(scanTrigger);
+        end
+        
+    end
+    
+end
+
